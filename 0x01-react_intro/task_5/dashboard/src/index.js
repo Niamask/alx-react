1680928dd9930a5d0c8a547a93613120c44885a3
Niamask <niamaskay@gmail.com>
2024-03-06 21:39:18 +0100

    9. Create a project using Webpack

---
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>React App</h1>
+      <p>This is a React app created from scratch.</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
