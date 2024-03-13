4792a4dff1e2253d9008bb664289bdba2a7956fc
Niamask <niamaskay@gmail.com>
2024-03-13 15:58:44 +0000

    update package.json and src/App/App.test.js

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
 import PropTypes from "prop-types";
 import "./App.css";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Login from "../Login/Login";
-import Notifications from "../Notifications/Notifications";
-import CourseList from "../CourseList/CourseList";
 import { getLatestNotification } from "../utils/utils";
 
 const listCourses = [
@@ -13,28 +13,30 @@ const listCourses = [
   { id: 2, name: "Webpack", credit: 20 },
   { id: 3, name: "React", credit: 40 },
 ];
+
 const listNotifications = [
   { id: 1, type: "default", value: "New course available" },
   { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+  { id: 3, type: "urgent", html: getLatestNotification() },
 ];
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Notifications listNotifications={listNotifications} />
         <div className="App">
-          <Header />
-          <div className="App-body">
-            {this.props.isLoggedIn ? (
-              <CourseList listCourses={listCourses} />
-            ) : (
-              <Login />
-            )}
+          <div className="flex-header">
+            <Notifications
+              displayDrawer
+              listNotifications={listNotifications}
+            />
+            <Header />
           </div>
-        </div>
-        <div className="App-footer">
+          {this.props.isLoggedIn ? (
+            <CourseList listCourses={listCourses} />
+          ) : (
+            <Login />
+          )}
           <Footer />
         </div>
       </React.Fragment>
