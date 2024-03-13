f2c99e32553b4092feed6cad78c21590c2c9afad
Niamask <niamaskay@gmail.com>
2024-03-13 15:00:29 +0000

    update task_0

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import Notifications from "../Notifications/Notifications";
-import Header from "../Header/Header";
-import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
-import CourseList from "../CourseList/CourseList";
 import PropTypes from "prop-types";
 import "./App.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import Notifications from "../Notifications/Notifications";
+import CourseList from "../CourseList/CourseList";
 import { getLatestNotification } from "../utils/utils";
 
 const listCourses = [
@@ -13,30 +13,28 @@ const listCourses = [
   { id: 2, name: "Webpack", credit: 20 },
   { id: 3, name: "React", credit: 40 },
 ];
-
 const listNotifications = [
   { id: 1, type: "default", value: "New course available" },
   { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html: getLatestNotification() },
+  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
 ];
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Notifications listNotifications={listNotifications} />
         <div className="App">
-          <div className="flex-header">
-            <Notifications
-              displayDrawer
-              listNotifications={listNotifications}
-            />
-            <Header />
+          <Header />
+          <div className="App-body">
+            {this.props.isLoggedIn ? (
+              <CourseList listCourses={listCourses} />
+            ) : (
+              <Login />
+            )}
           </div>
-          {this.props.isLoggedIn ? (
-            <CourseList listCourses={listCourses} />
-          ) : (
-            <Login />
-          )}
+        </div>
+        <div className="App-footer">
           <Footer />
         </div>
       </React.Fragment>
