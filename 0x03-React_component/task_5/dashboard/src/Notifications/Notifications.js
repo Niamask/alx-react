e17785c51c58c23f49ac06e1b115202b2528d99d
Niamask <niamaskay@gmail.com>
2024-03-14 15:35:20 +0000

    10. Make your own pure component

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./Notifications.css";
 import closeIcon from "../assets/close-icon.png";
-import { getLatestNotification } from "../utils/utils";
 import NotificationItem from "./NotificationItem";
 import PropTypes from "prop-types";
 import NotificationItemShape from "./NotificationItemShape";
 
-// function Notifications({ displayDrawer, listNotifications })
 class Notifications extends React.Component {
   constructor(props) {
     super(props);
     this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.length > this.props.listNotifications.length;
   }
 
   markAsRead(id) {
@@ -21,46 +23,39 @@ class Notifications extends React.Component {
     return (
       <React.Fragment>
         {this.props.displayDrawer ? (
-          <div>
+          <div className="flex-area">
             <div className="menuItem">
               <p>Your notifications</p>
             </div>
-
             <div className="Notifications">
-              <button
-                style={{
-                  color: "#3a3a3a",
-                  fontWeight: "bold",
-                  background: "none",
-                  border: "none",
-                  fontSize: "10px",
-                  position: "absolute",
-                  right: "1rem",
-                  top: "1rem",
-                  cursor: "pointer",
-                }}
-                aria-label="Close"
-                onClick={console.log("Close button has been clicked")}
-              >
-                <img src={closeIcon} alt="closeIcon" width="10px" />
-              </button>
-              <p>Here is the list of notifications</p>
               <ul>
                 {this.props.listNotifications &&
                 this.props.listNotifications.length > 0 ? (
                   this.props.listNotifications.map(
-                    ({ id, value, type, html }) => (
+                    ({ id, html, type, value }) => (
                       <NotificationItem
                         key={id}
                         markAsRead={this.markAsRead}
-                        value={value}
                         type={type}
+                        value={value}
                         html={html}
                       />
                     )
                   )
                 ) : (
-                  <NotificationItem value="No new notification for now" />
+                  <div className="notification-header">
+                    <NotificationItem value="No new notification for now" />
+                    <button
+                      aria-label="Close"
+                      onClick={console.log("Close button has been clicked")}
+                    >
+                      <img
+                        style={{ display: "inline" }}
+                        src={closeIcon}
+                        alt="Close"
+                      />
+                    </button>
+                  </div>
                 )}
               </ul>
             </div>
@@ -81,7 +76,7 @@ Notifications.propTypes = {
 };
 
 Notifications.defaultProps = {
-  displayDrawer: "false",
+  displayDrawer: false,
   listNotifications: [],
 };
 
